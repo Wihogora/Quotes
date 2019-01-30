64d0e0cce0b10d577011f4a1d2d698ae2d6a4359
Wihogora Wivine <wwihogora@gmail.com>
2019-01-30 17:55:53 +0200

    Auto-generated commit

--- src/app/quotesclass.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Quotesclass } from './quotesclass';
-
-describe('Quotesclass', () => {
-  it('should create an instance', () => {
-    expect(new Quotesclass(1,'')).toBeTruthy();
-  });
-});
--- src/app/quotesclass.ts
+++
@@ -1,6 +0,0 @@
-export class Quotesclass {
-    public showAuthor: Boolean
-    constructor(public id:number,public name:string, public quotes:string ,public upvotes:number,public downvotes:number,author:string,public completeDate:Date ){
-        this.showAuthor=false
-    }
-}
